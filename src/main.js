77d870e0af770c81b73366aaf9d91d3bd69cf3c3
mikefab <mikefabrikant@gmail.com>
2017-10-12 12:06:12 -0400

    Log less often

--- src/main.js
+++ src/main.js
@@ -67,7 +67,9 @@ function update_row(school, object, index) {
       if (err) throw err
       client.query(text, values, (err, res) => {
         done()
-        console.log(index, is_valid, school.country_code, object.school.id,  err)
+        if (index % 100 === 0)
+          console.log(index, is_valid, school.country_code, object.school.id,  err)
+        }
         resolve()
       })
     });
