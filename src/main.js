39eaf7786ce6615a0f9a3e615a616c9c1c34148f
mikefab <mikefabrikant@gmail.com>
2017-10-12 11:54:53 -0400

    Log out index

--- src/main.js
+++ src/main.js
@@ -67,7 +67,7 @@ function update_row(school, object, index) {
       if (err) throw err
       client.query(text, values, (err, res) => {
         done()
-        console.log(is_valid, school.country_code, object.school.id,  err)
+        console.log(index, is_valid, school.country_code, object.school.id,  err)
         resolve()
       })
     });
