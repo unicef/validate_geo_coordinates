24f2bc57120c36ff0a178bf2ecbe7a980421b788
Ubuntu <mfabrikant@project-connect.tnhb0fuarrrufbuqhvnmkxkvbd.bx.internal.cloudapp.net>
2017-10-13 20:25:27 +0000

    Compare iso 3 and 2 iso codes

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { Pool } from 'pg'
 // import Cursor from 'pg-cursor'
 import * as config from '../config'
 import bluebird from 'bluebird'
-
+import iso_3_2 from '../public/iso3_2'
 const pool_schools = new Pool(config.db_schools)
 const pool_countries = new Pool(config.db_countries)
 
@@ -26,6 +26,7 @@ function get_schools_to_geo_validate() {
     pool_schools.connect((err, client, done) => {
       if (err) throw err
       client.query('SELECT id, lat, lon, country_code from schools where date_geo_validated is null and lat is not null and lon is not null', [], (err, res) => {
+      //client.query("SELECT id,name, lat, lon, country_code from schools where lat is not null and lon is not null and country_code='BR'", [], (err, res) => {
         done()
         if (err) {
           console.log(err.stack)
@@ -41,7 +42,7 @@ function geo_validate_coordinates(school) {
   return new Promise((resolve, reject) => {
     pool_countries.connect((err, client, done) => {
       if (err) throw err
-      client.query("select count(*) from all_countries_one_table WHERE ST_Within (ST_Transform (ST_GeomFromText ('POINT(" + school.lon + " " + school.lat + ")',4326),4326), all_countries_one_table.geom);", [], (err, res) => {
+      client.query("select iso, name_0 from all_countries_one_table WHERE ST_Within (ST_Transform (ST_GeomFromText ('POINT(" + school.lon + " " + school.lat + ")',4326),4326), all_countries_one_table.geom);", [], (err, res) => {
         done()
         if (err) {
           console.log(err.stack)
@@ -59,7 +60,12 @@ function geo_validate_coordinates(school) {
 function update_row(school, object, index) {
   return new Promise((resolve, reject) => {
     const text = 'update schools set date_geo_validated = CURRENT_TIMESTAMP, coords_within_country = $1 where id = $2;'
-    let is_valid = object.rows[0].count > 0 && school.country_code === object.school.country_code;
+    let is_valid = false;
+    let iso = null;
+    if (object.rows.length > 0) {
+      iso = object.rows[0].iso.replace(/('|\s+)/g, '');
+      is_valid = object.rows.length > 0 && school.country_code === iso_3_2[iso];
+    }
     const values = [
       is_valid,
       object.school.id]
@@ -67,8 +73,8 @@ function update_row(school, object, index) {
       if (err) throw err
       client.query(text, values, (err, res) => {
         done()
-        if (index % 1 === 0) {
-          console.log(index, is_valid, school.country_code, object.school.id,  err)
+        if (index % 100 === 0) {
+          console.log(index, is_valid, school.country_code,iso, object.school.id,  err)
         }
         resolve()
       })
@@ -81,6 +87,7 @@ function validate_and_update(school, index) {
 
     geo_validate_coordinates(school)
     .then(row => {
+      // row is an object with school and country iso from all_countries_one_table
       update_row(school, row, index)
       .then(resolve)
     })
